efd1136e66bd24e560243ce93f370ee0924ff084
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-17 01:25:34 -0700

    Update continous_Ping.py

--- continous_Ping.py
+++ continous_Ping.py
@@ -1,35 +1,16 @@
-from pythonping import ping
+import subprocess
 import keyboard
-import threading
 
-class PingThread(threading.Thread):
-    def __init__(self, ip_address):
-        threading.Thread.__init__(self)
-        self.ip_address = ip_address
-        self.stop_event = threading.Event()
+def send_packets(ip_address):
+    hping_command = f"hping3 {ip_address} -d 76 -c 0"
+    process = subprocess.Popen(hping_command, shell=True)
 
-    def run(self):
-        while not self.stop_event.is_set():
-            response_list = ping(self.ip_address)
-            if response_list.rtt_avg_ms:
-                print(f"Ping to {self.ip_address}: {response_list.rtt_avg_ms} ms")
-            else:
-                print(f"Unable to ping {self.ip_address}")
-
-def ping_ip(ip_address):
-    threads = []
     while True:
-        thread = PingThread(ip_address)
-        threads.append(thread)
-        thread.start()
-
         if keyboard.is_pressed('esc'):
-            for thread in threads:
-                thread.stop_event.set()
-                thread.join()
+            process.terminate()
             break
 
 if __name__ == "__main__":
-    ip_address = input("Enter the IP address to ping: ")
-    print("Press the 'esc' key to stop pinging.")
-    ping_ip(ip_address)
+    ip_address = input("Enter the IP address to send packets: ")
+    print("Press the 'esc' key to stop sending packets.")
+    send_packets(ip_address)
