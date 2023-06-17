7cbc2e42380ea95b2fe32c834b28cbc0988deed5
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-17 01:31:19 -0700

    Update continous_Ping.py

--- continous_Ping.py
+++ continous_Ping.py
@@ -2,7 +2,7 @@ import subprocess
 import keyboard
 
 def send_packets(ip_address):
-    hping_command = f"hping3 {ip_address} -d 76 -c 0"
+    hping_command = f"hping3 {ip_address} -d 65535 -c 0"
     process = subprocess.Popen(hping_command, shell=True)
 
     while True:
@@ -14,3 +14,4 @@ if __name__ == "__main__":
     ip_address = input("Enter the IP address to send packets: ")
     print("Press the 'esc' key to stop sending packets.")
     send_packets(ip_address)
+
